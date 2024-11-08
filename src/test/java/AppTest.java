7eaa68c3ce7a1bb2358bb003326f7825866494d7
Mrinalika <dmrinalika@gmail.com>
2024-11-08 09:30:37 +0530

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+// src/test/java/AppTest.java
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
